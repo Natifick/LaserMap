330b55318135b864a16961326997d66a10140769
Natifick <natifick@yandex.ru>
2023-09-21 15:56:34 +0300

    non-working stuff with google directions api

--- app/src/main/java/com/example/lasermap/MapsActivity.kt
+++ app/src/main/java/com/example/lasermap/MapsActivity.kt
@@ -4,14 +4,19 @@ import android.Manifest
 import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.net.Uri
-import android.os.*
+import android.os.Build
+import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.lasermap.databinding.ActivityMapsBinding
+import com.google.android.gms.common.api.Response
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -19,10 +24,82 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import java.io.*
-import java.net.URL
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import com.google.gson.annotations.SerializedName
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.io.File
 import kotlin.concurrent.thread
 
+
+// Gson schema for project
+
+class DirectionResults {
+    @SerializedName("routes")
+    val routes: kotlin.collections.List<Route>? = null
+}
+class Route {
+    @SerializedName("overview_polyline")
+    val overviewPolyLine: OverviewPolyLine? = null
+    val legs: kotlin.collections.List<Legs>? = null
+}
+class Legs {
+    val steps: kotlin.collections.List<Steps>? = null
+}
+class Steps {
+    val start_location: Location? = null
+    val end_location: Location? = null
+    val polyline: OverviewPolyLine? = null
+}
+class OverviewPolyLine {
+    @SerializedName("points")
+    var points: String? = null
+}
+class Location {
+    val lat = 0.0
+    val lng = 0.0
+}
+
+
+object RouteDecode {
+    fun decodePoly(encoded: String): ArrayList<LatLng> {
+        val poly = ArrayList<LatLng>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].code - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].code - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+            val position = LatLng(lat.toDouble() / 1E5, lng.toDouble() / 1E5)
+            poly.add(position)
+        }
+        return poly
+    }
+}
 
 class MapsActivity: AppCompatActivity(), OnMapReadyCallback {
 
@@ -81,34 +158,109 @@ class MapsActivity: AppCompatActivity(), OnMapReadyCallback {
 
         // Check if we can access user's location
         checkLocationPermission()
-
         val parameters = "origin=${skoltech.latitude},${skoltech.longitude}&"+
-                         "destination=${redSquare.latitude},${redSquare.longitude}&"+
-                         "mode=bicycling&key=${R.string.google_maps_key}"
+                "destination=${redSquare.latitude},${redSquare.longitude}&"+
+                "mode=bicycling&key=${getResources().getString(R.string.google_maps_key)}"
+        Log.d("link", "https://maps.googleapis.com/maps/api/directions/json?${parameters}")
         thread(start = true) {
-            download("https://maps.googleapis.com/maps/api/directions/json?${parameters}",
-                applicationContext.getFilesDir())
+
+            download("https://maps.googleapis.com/",
+                skoltech, redSquare,
+                applicationContext.filesDir)
         }
+
     }
 
-//    fun downloadFile(link: String, outputFileName: String) {
-//        URL(link).openStream().use {
-//            Channels.newChannel(it).use { rbc ->
-//                FileOutputStream(outputFileName).use { fos ->
-//                    fos.channel.transferFrom(rbc, 0, Long.MAX_VALUE)
-//                }
+
+
+    private fun download(base_url: String, fromPosition: LatLng, toPosition: LatLng, path: File) {
+//        URL(link).openStream().use { input ->
+//            FileOutputStream(File(path, "/directions.json")).use { output ->
+//                input.copyTo(output)
 //            }
 //        }
-//    }
+//        val base_url = "http://maps.googleapis.com/"
 
-    fun download(link: String, path: File) {
-        URL(link).openStream().use { input ->
-            FileOutputStream(File(path, "/directions.json")).use { output ->
-                input.copyTo(output)
-            }
-        }
+        val gson = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
+
+        val retrofitBuilder = Retrofit.Builder()
+            .baseUrl(base_url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val reqinterface = retrofitBuilder.create(Api::class.java)
+
+        reqinterface.getJson(
+            "${fromPosition.latitude},${fromPosition.longitude}",
+            "${toPosition.latitude},${toPosition.longitude}",
+            getResources().getString(R.string.google_maps_key)
+        )?.enqueue(
+            object: Callback<DirectionResults?> {
+                override fun onResponse(
+                    call: Call<DirectionResults?>,
+                    directionResults: retrofit2.Response<DirectionResults?>
+                ) {
+                    Log.d("snet", call.request().toString())
+                    Log.d("err", directionResults.errorBody().toString())
+                    Log.d("results", directionResults.message().toString())
+                    Log.i("zacharia", "inside on success " + directionResults.body()!!.routes!!.size)
+                    val routelist = ArrayList<LatLng>()
+                    if (directionResults.body()!!.routes!!.isNotEmpty()) {
+                        var decodelist: ArrayList<LatLng>
+                        val routeA = directionResults.body()!!.routes!![0]
+                        Log.i("zacharia", "Legs length : " + routeA.legs!!.size)
+                        if (routeA.legs.isNotEmpty()) {
+                            val steps = routeA.legs[0].steps
+                            Log.i("zacharia", "Steps size :" + steps!!.size)
+                            var step: Steps
+                            var location: Location?
+                            var polyline: String?
+                            for (i in steps.indices) {
+                                step = steps[i]
+                                location = step.start_location
+                                routelist.add(LatLng(location!!.lat, location.lng))
+                                Log.i("zacharia", "Start Location :" + location.lat + ", " + location.lng)
+                                polyline = step.polyline!!.points
+                                decodelist = RouteDecode.decodePoly(polyline!!)
+                                routelist.addAll(decodelist)
+                                location = step.end_location
+                                routelist.add(LatLng(location!!.lat, location.lng))
+                                Log.i("zacharia", "End Location :" + location.lat + ", " + location.lng)
+                            }
+                        }
+                    }
+                    Log.i("zacharia", "routelist size : " + routelist.size)
+                    if (routelist.size > 0) {
+                        val rectLine = PolylineOptions().width(10f).color(
+                            Color.RED
+                        )
+                        for (i in routelist.indices) {
+                            rectLine.add(routelist[i])
+                        }
+//                        // Adding route on the map
+//                        mMap.addPolyline(rectLine)
+//                        markerOptions.position(toPosition)
+//                        markerOptions.draggable(true)
+//                        mMap.addMarker(markerOptions)
+                    }
+                }
+
+                override fun onFailure(call: Call<DirectionResults?>, t: Throwable) {
+                    Log.d("onFailure", t.message!!)
+                }
+            })
     }
 
+    interface Api {
+        @GET("/maps/api/directions/json")
+        fun getJson(
+            @Query("origin") origin: String?,
+            @Query("destination") destination: String?,
+            @Query("key") key: String?,
+        ): Call<DirectionResults?>?
+    }
 
     private var locationCallback: LocationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
